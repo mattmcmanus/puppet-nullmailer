720b6b308cce3727b317e3213eaf0ebd6dc4c0d2
Matt McManus <mattmcmanus@gmail.com>
2012-02-02 11:28:24 -0500

    Rewrite to be a little more intelligent

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ Set's up the gloriously simple nullmailer MTA
 
 ## Usage
 
-    nullmailer {
-      'smarthost' => 'mailrelay.example.com'
+    class { "nullmailer":
+      smarthost => 'mailrelay.example.com'
     }
     
 or, with feeling:
 
-    nullmailer {
-      'smarthost' => 'mailrelay.example.com'
-      'port' => '1450'
-      'user' => 'super@awesome'
-      'password' => 'ISwearToScience'
+    class { "nullmailer":
+      smarthost => 'mailrelay.example.com'
+      port => '1450'
+      user => 'super@awesome'
+      password => 'ISwearToScience'
     }
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,36 +1,55 @@
 class nullmailer(
   $smarthost,
-  $protocol,
-  $port,
-  $user,
-  $password
+  $protocol = undef,
+  $port = undef,
+  $user = undef,
+  $password = undef
 ) {
   
   # Install the package
   package {   
-    ['nullmailer']: ensure => installed
+    'nullmailer': ensure => installed
   }
   
+  package {   
+    'postfix': ensure => purged
+  }
+  
+  service {
+    'nullmailer': ensure => running
+  }
+  
+  $smarthost_real = $smarthost
+  
   if $protocol {
-    $smarthost << " "+$protocol
+    $protocol_real += $protocol
+  } else {
+    $protocol_real = 'smtp'
   }
   
   if $port {
-    $smarthost << " --port="+$port
+    $port_real += "--port=${port}"
+  } else {
+    $port_real = ''
   }
   
   if $user {
-    $smarthost << " --user="+$user
+    $user_real += "--user=${user}"
+  } else {
+    $user_real = ''
   }
   
   if $password {
-    $smarthost << " --password="+$password
+    $password_real += "--password=${password}"
+  } else {
+    $password_real = ''
   }
   
   file {
     "/etc/nullmailer/remotes":
       path => "/etc/nullmailer/remotes",
       content  => template("nullmailer/remotes.erb"),
+      notify => Service['nullmailer'],
       ensure => present;
   }
 
